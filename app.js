85c2eb25ccf416da8deca05042492659337b6de7
cindy correa <cindycorrea04@gmail.com>
2024-02-21 13:41:48 -0700

    updated dependences

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
+const session = require("express-session");
+const request = require("request-promise");
 
 const dotenv = require("dotenv").config();
 
@@ -14,6 +16,13 @@ app.use(bodyParser.json());
 app.use("/", require("./routes/builders"));
 app.use("/", require("./routes/sets"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  session({
+    secret: "your_secret_here",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
