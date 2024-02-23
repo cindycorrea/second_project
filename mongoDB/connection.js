80e668af7eb0b6e20ccc823b465e725d52132946
cindy correa <cindycorrea04@gmail.com>
2024-02-23 12:20:56 -0700

    Testing Auth

--- mongoDB/connection.js
+++ mongoDB/connection.js
@@ -6,11 +6,10 @@ mongoose.connect(process.env.URI);
 console.log('Connection successful.')
 
 // Close Mongoose connection on SIGINT (Ctrl+C)
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    console.log('Mongoose connection closed due to app termination');
-    process.exit(0);
-  });
+process.on("SIGINT", function () {
+  mongoose.connection.close();
+  console.log("You are cut off!");
+  process.exit(0);
 });
 
 const dbConnection = mongoose.connection;
--- routes/builders.js
+++ routes/builders.js
@@ -1,11 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { requiresAuth } = require("express-openid-connect");
+const buildersController = require("../controllers/builders");
 
-const buildersController = require('../controllers/builders');
-
-router.get('/builders', buildersController.getAllBuilders);
-router.get('/builders/:id', buildersController.getBuilder);
-router.post('/builders', buildersController.newBuilder);
-router.put('/builders/:id', buildersController.updateBuilder);
-router.delete('/builders/:id', buildersController.deleteBuilder);
+router.get("/builders", buildersController.getAllBuilders);
+router.get("/builders/:id", buildersController.getBuilder);
+router.post("/builders", requiresAuth(), buildersController.newBuilder);
+router.put("/builders/:id", requiresAuth(), buildersController.updateBuilder);
+router.delete(
+  "/builders/:id",
+  requiresAuth(),
+  buildersController.deleteBuilder
+);
 
 module.exports = router;
